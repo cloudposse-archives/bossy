97f894c53b819dc1cdb138eb8cf12db322fa55ed
Matt Calhoun <matt@cloudposse.com>
2021-11-22 22:45:21 -0500

    feat: add crypto lib (#3)

---
+++ libs/crypto/README.md
@@ -0,0 +1,7 @@
+# crypto
+
+This library contains various functionality for working with cryptography, such as verify GitHub and Spacelift payloads.
+
+## Running unit tests
+
+Run `nx test crypto` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/crypto/src/lib/crypto.spec.ts
@@ -0,0 +1,55 @@
+import { compareHMACSHA1Signature, createHMACSHA1Signature } from "./crypto";
+
+const secret = "ThisIsMySecretKey";
+const message = "This is my plaintext message";
+const calculatedSignature = "00e04f75727b3789014d5e7b90309341aaf425dd"; // https://www.freeformatter.com/hmac-generator.html
+
+describe("crypto lib", () => {
+  describe("createHMACSignature", () => {
+    describe("green path", () => {
+      it("generates a valid hmac-sha1 signature", () => {
+        const signature = createHMACSHA1Signature(secret, message);
+        expect(signature).toBe(calculatedSignature);
+      });
+    });
+  });
+
+  describe("compareHMACSignature", () => {
+    describe("green path", () => {
+      it("returns true", () => {
+        const result = compareHMACSHA1Signature(
+          secret,
+          message,
+          calculatedSignature
+        );
+
+        expect(result).toBe(true);
+      });
+    });
+
+    describe("red path", () => {
+      it("returns false with different key", () => {
+        const secret = "ThisIsMySecretKeyAltered";
+
+        const result = compareHMACSHA1Signature(
+          secret,
+          message,
+          calculatedSignature
+        );
+
+        expect(result).toBe(false);
+      });
+      it("returns false with different message", () => {
+        const message = "This is my plaintext message altered";
+
+        const result = compareHMACSHA1Signature(
+          secret,
+          message,
+          calculatedSignature
+        );
+
+        expect(result).toBe(false);
+      });
+    });
+  });
+});
