f3f0c45c4f2d1dfa8c00525b43fefb54fafce8e3
Matt Calhoun <matt@mattcalhoun.com>
2022-05-12 15:15:23 -0400

    revert bodyparer experiment

--- apps/bossy/src/app/app.ts
+++ apps/bossy/src/app/app.ts
@@ -1,5 +1,4 @@
 import { sendSpaceliftEventsToSlackRouter } from "@cloudposse/modules/spacelift";
-import bodyParser from "body-parser";
 import express from "express";
 import helmet from "helmet";
 
@@ -8,8 +7,7 @@ process.env["NODE_CONFIG_DIR"] = process.cwd() + "/config/";
 const app = express();
 
 app.use(helmet());
-//app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/test", async (req, res) => {
